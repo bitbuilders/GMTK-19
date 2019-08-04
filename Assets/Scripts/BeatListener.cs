31a26878b09874ab7213555a93daea8015b8b8b6
Colin Misbach <colin@misbach.com>
2019-08-04 00:17:43 -0600

    Added beat listener to visually show if in beat

---
+++ Assets/Scripts/BeatListener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeatListener : MonoBehaviour
+{
+    [SerializeField] Image m_Image = null;
+    [SerializeField] Color m_Collect= Color.yellow;
+    [SerializeField] Color m_OffRhythm = Color.black;
+
+    private void Update()
+    {
+        if (WarriorBeat.Instance.IsInBeat()) m_Image.color = m_Collect;
+        else m_Image.color = m_OffRhythm;
+    }
+}
